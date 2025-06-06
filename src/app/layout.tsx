c9de8ea869c9c9b56d14ca07538179f07740ddc9
Victor Winsjansen <victor.winsjansen@icloud.com>
2025-06-06 17:24:14 +0700

    Added temporary favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,22 @@ const caladea = Caladea({
 export const metadata: Metadata = {
   title: "Victor Winsjansen | Portfolio",
   description: "Portfolio",
+  icons: {
+    icon: [
+      { url: "/assets/favicons/favicon.ico" },
+      {
+        url: "/assets/favicons/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/assets/favicons/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: "/assets/favicons/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
